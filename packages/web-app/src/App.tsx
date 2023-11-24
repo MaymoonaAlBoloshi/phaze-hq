23632a9542f1aed10c324b46bb6d1ed36df51415
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-11-24 19:53:44 +0400

    get users from BE

--- packages/web-app/src/App.tsx
+++ packages/web-app/src/App.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useUsers } from "./dataHooks/users";
+import { IUser } from "types/src/user";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const { data: users, isLoading, error } = useUsers();
 
-	return (
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
+
+  return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
         Welcome to the App
       </h1>
-      
+
       <button className="btn">Hello daisyUI</button>
+      <div>
+        {users.map((user: IUser) => <p key={user.id}>{user.name}</p>)}
+      </div>
     </div>
-	);
+  );
 }
 
 export default App;
---
+++ packages/web-app/src/dataHooks/users.ts
@@ -0,0 +1,20 @@
+import axiosInstance from "../configs/api.ts";
+import { useQuery } from "react-query";
+
+const fetchUsers = async () => {
+  const { data } = await axiosInstance.get("/users");
+  return data;
+};
+
+export const useUsers = () => {
+  return useQuery("users", fetchUsers);
+};
+
+const fetchSingleUser = async (id: string) => {
+  const { data } = await axiosInstance.get(`/users/${id}`);
+  return data;
+};
+
+export const useSingleUser = (id: string) => {
+  return useQuery(["users", id], () => fetchSingleUser(id));
+};
